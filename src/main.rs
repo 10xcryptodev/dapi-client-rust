84e2dd423dcb71df5e9336cd01704abf4cfd121f
10xcryptodev <10xcryptodev@gmail.com>
2020-06-16 00:10:08 -0300

    getBestBlockHash

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate jsonrpc;
+extern crate serde_derive;
+extern crate serde;
+
 fn main() {
-    println!("Hello, world!");
+    let client = jsonrpc::client::Client::new("http://seed.evonet.networks.dash.org:3000".to_owned(), None, None);
+    let request = client.build_request("getBestBlockHash", &[]);
+    match client.send_request(&request).and_then(|res| res.into_result::<String>()) {
+        Ok(result) => println!("getBestBlockHash {}", result),
+        Err(e) => println!("error {}", e),
+    }
 }
