88503bea760af6440a9be9fba61033a0256032f2
10xcryptodev <10xcryptodev@gmail.com>
2020-07-22 08:26:48 -0300

    wip json rpc with parameters

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
 extern crate jsonrpc;
-extern crate serde_derive;
 extern crate serde;
+#[macro_use] extern crate serde_derive;
+
+use serde_json::json;
+
+#[derive(Serialize)]
+struct GetBlockHashParameter {
+    height : i32,
+}
 
 fn main() {
     let client = jsonrpc::client::Client::new("http://seed.evonet.networks.dash.org:3000".to_owned(), None, None);
-    let request = client.build_request("getBestBlockHash", &[]);
+    let mut request = client.build_request("getBestBlockHash", &[]);
     match client.send_request(&request).and_then(|res| res.into_result::<String>()) {
         Ok(result) => println!("getBestBlockHash {}", result),
+        Err(e) => println!("error {}", e),
+    }
+
+    let param = GetBlockHashParameter { height : 1 };
+    let get_block_hash_parameter = vec![json!({"params" : param})];
+
+    request = client.build_request("getBlockHash", &get_block_hash_parameter);
+    //println!("params {}", request.params[0]);
+    match client.send_request(&request).and_then(|res| res.into_result::<String>()) {
+        Ok(result) => println!("getBlockHash {}", result),
         Err(e) => println!("error {}", e),
     }
 }
