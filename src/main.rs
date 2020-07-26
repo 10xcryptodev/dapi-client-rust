c799ce507a51c83cb799d492ded85576f945fd30
10xcryptodev <10xcryptodev@gmail.com>
2020-07-25 23:36:50 -0300

    remove nonce mismatch

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,28 @@ extern crate serde;
 
 use serde_json::json;
 
-#[derive(Serialize)]
+#[derive(Serialize,Deserialize)]
 struct GetBlockHashParameter {
     height : i32,
 }
 
 fn main() {
-    let client = jsonrpc::client::Client::new("http://seed.evonet.networks.dash.org:3000".to_owned(), None, None);
-    let mut request = client.build_request("getBestBlockHash", &[]);
+    let client = jsonrpc::client::Client::new("http://seed.evonet.networks.dash.org:3000".to_owned(), None);
+    let mut request = client.build_request("getBestBlockHash", json!(""));
+    request.id = json!(1);
     match client.send_request(&request).and_then(|res| res.into_result::<String>()) {
         Ok(result) => println!("getBestBlockHash {}", result),
         Err(e) => println!("error {}", e),
     }
 
-    let param = GetBlockHashParameter { height : 1 };
-    let get_block_hash_parameter = vec![json!({"params" : param})];
+    let param = GetBlockHashParameter { height : 5 };
+    let get_block_hash_parameter = json!(param);
 
-    request = client.build_request("getBlockHash", &get_block_hash_parameter);
+    request = client.build_request("getBlockHash", get_block_hash_parameter);
     //println!("params {}", request.params[0]);
     match client.send_request(&request).and_then(|res| res.into_result::<String>()) {
         Ok(result) => println!("getBlockHash {}", result),
         Err(e) => println!("error {}", e),
     }
+    
 }
